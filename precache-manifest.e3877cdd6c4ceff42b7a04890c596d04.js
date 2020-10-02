7c28d1351840875a4a3541ea92c9bf4be1790eec
unknown <draz@ualberta.ca>
2020-10-02 14:59:51 -0600

    Updates

---
+++ precache-manifest.e3877cdd6c4ceff42b7a04890c596d04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2036ee2c9e03b1d96083c5e8783cfced",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "2931ed6c4745a280cad9",
+    "url": "/project-4-assessment/static/css/main.dbb2b88e.chunk.css"
+  },
+  {
+    "revision": "6e42dd1542503a26f07c",
+    "url": "/project-4-assessment/static/js/2.e68b043b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-4-assessment/static/js/2.e68b043b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2931ed6c4745a280cad9",
+    "url": "/project-4-assessment/static/js/main.77ee884b.chunk.js"
+  },
+  {
+    "revision": "c60d7824db1a1daa4589",
+    "url": "/project-4-assessment/static/js/runtime-main.1eac270b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.72a42921075fd55a34907e1662e82473.js"
+  "/project-4-assessment/precache-manifest.e3877cdd6c4ceff42b7a04890c596d04.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/project-4-assessment/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });

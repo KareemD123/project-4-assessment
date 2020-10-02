f27a17dd543a79c99c02777bc35440f00fb39cc9
unknown <draz@ualberta.ca>
2020-10-02 14:53:31 -0600

    Updates

---
+++ precache-manifest.72a42921075fd55a34907e1662e82473.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d19ac6d40df797147906591511b0d094",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aad8f530d09eb9ddb4f3",
+    "url": "/static/css/main.dbb2b88e.chunk.css"
+  },
+  {
+    "revision": "6e42dd1542503a26f07c",
+    "url": "/static/js/2.e68b043b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e68b043b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aad8f530d09eb9ddb4f3",
+    "url": "/static/js/main.39d4354a.chunk.js"
+  },
+  {
+    "revision": "97b258f68aad10e2d918",
+    "url": "/static/js/runtime-main.beb1b2f6.js"
+  }
+]);
